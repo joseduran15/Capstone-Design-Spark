29f9f85219c1a99eefeb7d3bc90146f417d9a58d
axcleri <angelaguria7@gmail.com>
2022-11-06 22:13:07 -0500

    basic upload location to database

--- Spark/Spark/ViewController.swift
+++ Spark/Spark/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 import MapKit
 import CoreLocation
+import FirebaseCore
+import FirebaseAnalytics
+import FirebaseAnalyticsSwift
+import FirebaseDatabase
+import FirebaseDatabaseSwift
 
 class ViewController: UIViewController, CLLocationManagerDelegate{
     
     var locationManager = CLLocationManager()
+    var ref: DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        ref = Database.database().reference()
     }
 
     @IBOutlet weak var coordinates: UILabel!
@@ -36,6 +43,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         let coordinations = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude,longitude: userLocation.coordinate.longitude)
         
         coordinates.text = CoordToString(location: coordinations)
+        
+        var dataDictionary: [String: Any] = [:]
+        dataDictionary["Lat"] = String(format : "%f", coordinations.latitude)
+        dataDictionary["Long"] =  String(format : "%f", coordinations.longitude)
+        ref.setValue(dataDictionary)
         
         
     }
