87017fbdd4a31daab25dd7f65594e3fcb11a2a50
axcleri <angelaguria7@gmail.com>
2023-02-22 17:19:05 -0500

    fixed matching to work with real users, sort more effectively with observers

--- Spark/Spark/MatchViewController.swift
+++ Spark/Spark/MatchViewController.swift
@@ -20,16 +20,19 @@ class MatchViewController: UIViewController, CLLocationManagerDelegate
     var latData1 = 0.0
     var longData1 = 0.0
     
-    var currDisplayed = 0
+    var currDisplayed = ""
     var me = User()
     
     //gonna change to storing all this stuff in coredata
-    var liked: [Int] = []
-    var unLiked: [Int] = []
-    var notInRange: [Int] = []
+    var liked: [String] = []
+    var unLiked: [String] = []
+    
+    var appUsers: [String] = []
+    
+    @IBOutlet weak var imageDisplay: UIImageView!
+    
     
     /*ideas for how to get compatible users from database
-    - sort database somehow and grab from a range (going to SUCK to keep the ranges updated so probably not practical)
     - use the index grouping method from the firebase website: same level as users there's a gender and then an orientation and people's usernames are stored in their respective gender/orientation, and this could be expanded for interests too, then when looking for a potential match you look in the gender/orientation group https://firebase.google.com/docs/database/ios/structure-data#fanout
     */
     
@@ -38,28 +41,30 @@ class MatchViewController: UIViewController, CLLocationManagerDelegate
         print(ViewController.GlobalLoc.myLat)
         print(ViewController.GlobalLoc.myLong)
         ref = Database.database().reference()
-        var next = Int.random(in: 0..<999)
-        while(notInRange.contains(next) || unLiked.contains(next) || liked.contains(next))
-        {
-            next = Int.random(in: 0..<999)
-        }
-        displayNextUser(otherID: next)
+        userObserver()
     }
     
     @IBOutlet weak var display: UILabel!
     
-    func displayNextUser(otherID: Int){
-        ref = Database.database().reference().child("users").child(
-            String(otherID))
-        currDisplayed = otherID
-        ref.getData(completion:  { error, snapshot in
-            guard error == nil else {
-                print("issue")
-                return;
-            }
+    func displayNextUser(){
+        
+        /*appUsers.append("9")
+        appUsers.append("18")
+        appUsers.append("784")*/
+        print("appUsers count: \(appUsers.count)")
+        var next = Int.random(in: 0..<appUsers.count)
+        while(unLiked.contains(appUsers[next]) || liked.contains(appUsers[next]))
+        {
+            next = Int.random(in: 0..<appUsers.count)
+        }
+        currDisplayed = appUsers[next]
+        print("appUsers[next]: \(appUsers[next])")
+        ref = Database.database().reference().child("users").child(appUsers[next])
+        ref.observeSingleEvent(of: .value, with: { snapshot in 
             var a: [String: Any] = [:]
             //turning datasnapshot returned from database into a dictionary
-            a = snapshot?.value as! Dictionary<String, Any>
+            a = snapshot.value as! Dictionary<String, Any>
+            print("a: \(a)")
             //assigning values from the dictionary to variables so we don't have to type all the necessary error stuff every time
             self.latData1 = (a["locData"] as? [String:Any])?["lat"] as? Double ?? -1
             self.longData1 = (a["locData"] as? [String:Any])?["long"] as? Double ?? -1
@@ -69,16 +74,12 @@ class MatchViewController: UIViewController, CLLocationManagerDelegate
             var otherName = a["name"]
             //calculate distance between this user and other
             var distance = ViewController.GlobalLoc.distanceCalc(lat1: self.latData1, long1: self.longData1, lat2: ViewController.GlobalLoc.myLat, long2: ViewController.GlobalLoc.myLong)
-            //checks if the distance is less than 2 miles and if the gender/orientation of this user and other user are compatible
-            /*if(distance < 2 && (self.me.gender == otherOrien || otherOrien == "All") && (self.me.orientation == otherGen || self.me.orientation == "All")){
-                self.display.text = otherGen
-            }
-            else if (distance > 50)
-            {
-                self.notInRange.append(otherID)
-            }*/
+            //checks if the distance is less than 2 miles
             
-            self.display.text = otherName as! String
+            self.display.text = otherName as? String
+            print("othername: \(otherName)")
+            
+            //display other user's selfie
             
             
         });
@@ -87,32 +88,44 @@ class MatchViewController: UIViewController, CLLocationManagerDelegate
     
     @IBAction func matched(_ sender: Any){
         liked.append(currDisplayed)
-        var next = Int.random(in: 0..<999)
-        while(notInRange.contains(next) || unLiked.contains(next) || liked.contains(next))
-        {
-            next = Int.random(in: 0..<999)
-        }
-        displayNextUser(otherID: next)
+        displayNextUser()
     }
     
     @IBAction func noMatch(_ sender: Any){
         unLiked.append(currDisplayed)
-        var next = Int.random(in: 0..<999)
-        while(notInRange.contains(next) || unLiked.contains(next) || liked.contains(next))
-        {
-            next = Int.random(in: 0..<999)
-        }
-        displayNextUser(otherID: next)
+        displayNextUser()
         
     }
     
     @IBAction func testingMethod(_ sender: Any) {
-        var next = Int.random(in: 0..<999)
-        while(notInRange.contains(next) || unLiked.contains(next) || liked.contains(next))
-        {
-            next = Int.random(in: 0..<999)
-        }
-        displayNextUser(otherID: next)
+        displayNextUser()
+    }
+    
+    func userObserver()
+    {
+        ref.child("users").observe(DataEventType.childAdded, with: { snapshot in
+            
+            var a: [String: Any] = [:]
+            //turning datasnapshot returned from database into a dictionary
+            a = snapshot.value as! Dictionary<String, Any>
+            var otherGen = (a["gendData"] as? [String:Any])?["gender"] as? String ?? "error"
+            var otherOrien = (a["gendData"] as? [String:Any])?["orientation"] as? String ?? "error"
+            if(self.me.orientation == "All" || self.me.orientation == otherGen || otherGen == "Nonbinary")
+            {
+                if(otherOrien == "All" || otherOrien == self.me.gender || self.me.gender == "Nonbinary")
+                {
+                    self.appUsers.append(snapshot.key)
+                    print(snapshot.key)
+                    print(otherGen)
+                    print(otherOrien)
+                    
+                }
+            }
+            print("it ran")
+            
+        })
+        
+        
     }
     
     
